b8e11da87c0d71937596c8e6f32219481d76a881
shubhamkar <shubhamkar.nalkar@gmail.com>
2024-03-18 10:57:48 +0530

    change in audio player

--- lib/view/audi_player_widget.dart
+++ lib/view/audi_player_widget.dart
@@ -1,10 +1,10 @@
 import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
-import 'package:just_audio_background/just_audio_background.dart';
 import 'package:morning_vibes/common/extensions/greetings_for_date.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:flutter/services.dart';
 
 import '../common/constants_app.dart';
 import '../controller/audio_controllers.dart';
@@ -17,67 +17,10 @@ class AudiPlayerWidget extends StatefulWidget {
   State<AudiPlayerWidget> createState() => _AudiPlayerWidgetState();
 }
 
-class _AudiPlayerWidgetState extends State<AudiPlayerWidget> {
+class _AudiPlayerWidgetState extends State<AudiPlayerWidget>
+    with WidgetsBindingObserver {
   late AudioPlayer _audioPlayer;
   final String _greeting = DateTime.now().getGreetingFromHour();
-  final _playlist = ConcatenatingAudioSource(children: [
-    AudioSource.asset(
-      'assets/audio_for_papa/1Shree Ram Raksha Stotra.mp3',
-      tag: const MediaItem(
-        id: '1',
-        title: '1RamRaksha',
-      ),
-    ),
-    AudioSource.asset(
-      'assets/audio_for_papa/2Kalbhairavashtak.mp3',
-      tag: const MediaItem(
-        id: '2',
-        title: '2Kalbhairavashtak',
-      ),
-    ),
-    AudioSource.asset(
-      'assets/audio_for_papa/3Bhajans_by_Jagjit_Singh.mp3',
-      tag: const MediaItem(
-        id: '3',
-        title: '3Bhajans_by_Jagjit_Singh',
-      ),
-    ),
-//  "https://ve31.aadika.xyz/download/-_axSlApc98/mp3/128/1707657932/b6def78cd8dae345b6fcc1f606c9345c1271ce4874de986c9d26169436ac1436/1?f=x2mate.com",
-//       "https://ve34.aadika.xyz/download/_CQDgICh-dk/mp3/128/1707658137/dca0e6aa4506d5dcde25e9215fd228600afb64e2bf4491be936f764f86fcad21/1?f=x2mate.com",
-//       "https://ve61.aadika.xyz/download/QTAoyGByWok/mp3/128/1707658289/4f7c3651561fe4c50dc5a1229da1d012f774e74753f59ecc749e9083176f0417/1?f=x2mate.com"
-
-    // AudioSource.uri(
-    //   Uri.parse(
-    //       'https://ve31.aadika.xyz/download/-_axSlApc98/mp3/128/1707657932/b6def78cd8dae345b6fcc1f606c9345c1271ce4874de986c9d26169436ac1436/1?f=x2mate.com'),
-    //   tag: MediaItem(
-    //     id: "1",
-    //     title: "Unknown",
-    //     artist: "Unknown",
-    //     artUri: Uri.parse(""),
-    //   ),
-    // ),
-
-    // AudioSource.uri(
-    //   Uri.parse(
-    //       'https://ve34.aadika.xyz/download/_CQDgICh-dk/mp3/128/1707658137/dca0e6aa4506d5dcde25e9215fd228600afb64e2bf4491be936f764f86fcad21/1?f=x2mate.com'),
-    //   tag: MediaItem(
-    //     id: "1",
-    //     title: "Unknown",
-    //     artist: "Unknown",
-    //     artUri: Uri.parse(""),
-    //   ),
-    // ),
-    // AudioSource.uri(
-    //   Uri.parse(
-    //       'https://ve61.aadika.xyz/download/QTAoyGByWok/mp3/128/1707658289/4f7c3651561fe4c50dc5a1229da1d012f774e74753f59ecc749e9083176f0417/1?f=x2mate.com'),
-    //   tag: MediaItem(
-    //     id: "1",
-    //     title: "Unknown",
-    //     artist: "Unknown",
-    //     artUri: Uri.parse(""),
-    //   ),
-    // ),
-  ]);
   Stream<PositionData> get positionDataStream =>
       Rx.combineLatest3<Duration, Duration, Duration?, PositionData>(
         _audioPlayer.positionStream,
@@ -149,13 +92,22 @@ class _AudiPlayerWidgetState extends State<AudiPlayerWidget> {
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(10),
                 child: assetImage.isEmpty
-                    ? CachedNetworkImage(
+                    // ? CachedNetworkImage(
+                    //     fit: BoxFit.cover,
+                    //     imageUrl: ConstantsApp.defaultImageUrl,
+                    //     placeholder: (context, url) =>
+                    //         const CircularProgressIndicator(),
+                    //     errorWidget: (context, url, error) =>
+                    //         const Icon(Icons.error),
+                    //   )
+                    ? ExtendedImage.network(
+                        ConstantsApp.defaultImageUrl,
                         fit: BoxFit.cover,
-                        imageUrl: ConstantsApp.defaultImageUrl,
-                        placeholder: (context, url) =>
-                            const CircularProgressIndicator(),
-                        errorWidget: (context, url, error) =>
-                            const Icon(Icons.error),
+                        cache: true,
+                        // border: Border.all(color: Colors.red, width: 1.0),
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(30.0),
+                        ),
                       )
                     : Image.asset(
                         assetImage,
@@ -207,11 +159,6 @@ class _AudiPlayerWidgetState extends State<AudiPlayerWidget> {
 
   Future _init() async {
     await _audioPlayer.setLoopMode(LoopMode.all);
-    await _audioPlayer.setAudioSource(_playlist);
-    // await _audioPlayer.dynamicSetAll([
-    //   "https://ve31.aadika.xyz/download/-_axSlApc98/mp3/128/1707657932/b6def78cd8dae345b6fcc1f606c9345c1271ce4874de986c9d26169436ac1436/1?f=x2mate.com",
-    //   "https://ve34.aadika.xyz/download/_CQDgICh-dk/mp3/128/1707658137/dca0e6aa4506d5dcde25e9215fd228600afb64e2bf4491be936f764f86fcad21/1?f=x2mate.com",
-    //   "https://ve61.aadika.xyz/download/QTAoyGByWok/mp3/128/1707658289/4f7c3651561fe4c50dc5a1229da1d012f774e74753f59ecc749e9083176f0417/1?f=x2mate.com"
-    // ]);
+    // await _audioPlayer.setAudioSource();
   }
 }
